32e0fa4932c0fb3561a297b4f74138c686dd0429
Sir Francis Billard <sirfrancisbillard@gmail.com>
2016-11-30 13:06:18 -1000

    even bored-er

--- js/fp.js
+++ js/fp.js
@@ -1,1 +1,19 @@
-// insert meme here
+// insert meme about functional programming here
+
+function Id(a)
+{
+	return a // USEFUL
+}
+
+function Compose(f, b)
+{
+	return a(b)
+}
+
+function Curry(f, a)
+{
+	return function(b)
+	{
+		return f(a, b)
+	}
+}

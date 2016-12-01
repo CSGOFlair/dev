960e7ebe0491d224ab3f927de35c282f8a1903cb
Sir Francis Billard <sirfrancisbillard@gmail.com>
2016-11-30 22:05:45 -1000

    fp cleanup 1

--- js/fp.js
+++ js/fp.js
@@ -1,4 +1,4 @@
-// insert meme about functional programming here
+// crappy rip-off of fn.lua, only worse because billard wrote it
 
 function Id(a)
 {
@@ -8,6 +8,14 @@ function Id(a)
 function List(a)
 {
 	return [a];
+}
+
+function Flip(f)
+{
+	return function(a, b)
+	{
+		return f(b, a);
+	};
 }
 
 function Compose(f, a)
@@ -26,23 +34,31 @@ function Curry(f)
 	};
 }
 
-function Map(t, a, f) {
-	t += a[0];
-	f(a[0])
-	var remainingList = a.slice(1);
-	if (remainingList.length > 0) {
-		return Map(t, remainingList); 
+// Run a function over every value in an array
+// Example:
+// Map([1, 2, 3, 4, 5], function(num){return num * num}, 0) = [1, 4, 9, 16, 25]
+function Map(arr, func, total)
+{
+	total += arr[0];
+	func(arr[0])
+	var remains = arr.slice(1);
+	if (remains.length > 0) {
+		return Map(remains, func, total); 
 	}
 }
 
-function Sum(a)
+// Retrieve the sum of multiple numbers through wonky recursion
+// Must be terminated with a null parameter
+// Can also be used to curry numbers
+// Example:
+// Sum(1)(2)(3)() = 1 + 2 + 3 = 6
+function Sum(num)
 {
-	var sum = 0;
-	function sum_h(n)
+	function sum_h(num)
 	{
-		if (type(n) == "number")
+		if (typeof num == "number")
 		{
-			sum = sum + n;
+			sum = sum + num;
 			return sum_h;
 		}
 		else
@@ -50,5 +66,21 @@ function Sum(a)
 			return sum;
 		}
 	}
-  	return sum_h(a);
+  	return sum_h(pre);
+}
+
+// Retrieve a number's factorial through recursion
+// Factorials are the product of a number and every number between it and 1
+// Example:
+// Factorial(6) = 6 * 5 * 4 * 3 * 2 = 720
+function Factorial(num)
+{
+	if (num <= 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return (num * Factorial(a - 1));
+	}
 }

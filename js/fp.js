7df62ecaa848501c12e7b20b6a5a70f2ffe8b175
Sir Francis Billard <sirfrancisbillard@gmail.com>
2016-11-30 14:51:46 -1000

    Update fp.js

--- js/fp.js
+++ js/fp.js
@@ -2,18 +2,53 @@
 
 function Id(a)
 {
-	return a // USEFUL
+	return a;
 }
 
-function Compose(f, b)
+function List(a)
 {
-	return a(b)
+	return [a];
 }
 
-function Curry(f, a)
+function Compose(f, a)
 {
-	return function(b)
+	return f(a);
+}
+
+function Curry(f)
+{
+	return function(a)
 	{
-		return f(a, b)
+		return function(b)
+		{
+			return f(a, b);
+		};
+	};
+}
+
+function Map(t, a, f) {
+	t += a[0];
+	f(a[0])
+	var remainingList = a.slice(1);
+	if (remainingList.length > 0) {
+		return Map(t, remainingList); 
 	}
+}
+
+function Sum(a)
+{
+	var sum = 0;
+	function sum_h(n)
+	{
+		if (type(n) == "number")
+		{
+			sum = sum + n;
+			return sum_h;
+		}
+		else
+		{
+			return sum;
+		}
+	}
+  	return sum_h(a);
 }
